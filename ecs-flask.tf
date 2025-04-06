9a7741d097b8ddc4d103df45b58745c9edb84d67
aaron lim <aaron.limse@hotmail.com>
2025-04-06 22:39:04 +0800

    added internet gateway to ecs-flask.tf

--- ecs-flask.tf
+++ ecs-flask.tf
@@ -32,6 +32,20 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+# Public Route Table with Route to IGW
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "aalimsee-public-rt"
+  }
+}
+
 resource "aws_security_group" "ecs_sg" {
   name        = "flask-app-sg"
   description = "Allow HTTP and HTTPS"
