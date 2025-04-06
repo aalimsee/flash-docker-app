571a806a6066d32fa8429bca59b97e4510bc4c9d
aaron lim <aaron.limse@hotmail.com>
2025-04-06 22:42:27 +0800

    added internet gateway to ecs-flask.tf

--- ecs-flask.tf
+++ ecs-flask.tf
@@ -46,6 +46,11 @@ resource "aws_route_table" "public" {
   }
 }
 
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_security_group" "ecs_sg" {
   name        = "flask-app-sg"
   description = "Allow HTTP and HTTPS"
