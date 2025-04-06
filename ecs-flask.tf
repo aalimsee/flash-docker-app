15d6034d35e7b4c7bb7bcac626b0e789e2d5fe49
aaron lim <aaron.limse@hotmail.com>
2025-04-06 21:11:51 +0800

    added ecs-flask.tf

--- ecs-flask.tf
+++ ecs-flask.tf
@@ -22,8 +22,8 @@ resource "aws_security_group" "ecs_sg" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
